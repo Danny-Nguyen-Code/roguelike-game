dea6553306631d410a0e7daf34f2e3cdf7326c37
Danny Nguyen <204911897+Danny-Nguyen-Code@users.noreply.github.com>
2026-03-26 21:39:55 -0700

    Change dungeon GameMap call to include entities in procgen.py

--- procgen.py
+++ procgen.py
@@ -77,7 +77,7 @@ def generate_dungeon(max_rooms: int, room_min_size: int, room_max_size: int,
     map_width: int, map_height: int, player: Entity,
 ) -> GameMap:
     """Generate new dungeon map"""
-    dungeon = GameMap(map_width, map_height)
+    dungeon = GameMap(map_width, map_height, entities=[player])
 
     rooms: List[RectangularRoom] = []
 
